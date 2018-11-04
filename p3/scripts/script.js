7f0fe54e4f7ce90dc3487bf024b5982b9a180173
powni <magnusp99@hotmail.com>
2018-11-04 21:20:04 +0100

    navbar1

--- p3/scripts/script.js
+++ p3/scripts/script.js
@@ -1,9 +1,26 @@
 //---------- NAVBAR ----------
+const logo = document.getElementById('logo');
+const site = window.location.href;
 
 
+/*
+logo.addEventListener('click', () => {
+  //Sjekker hvilken side man er på. Byttes til "folk.ntnu...osv"
+  if (site !== 'index.html') {
+    console.log('Du føres til siden index.html') //Endrer til at man føres til index.html
+  }
+})
+*/
 
-
-
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
 
 //----------------------------
