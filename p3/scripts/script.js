1fc74bde1ce7b556ccac9561a240118991fa0e3a
powni <magnusp99@hotmail.com>
2018-11-05 19:01:23 +0100

    Added navbar to menu

--- p3/scripts/script.js
+++ p3/scripts/script.js
@@ -1,26 +1,28 @@
 //---------- NAVBAR ----------
 const logo = document.getElementById('logo');
 const site = window.location.href;
+const menuButton = document.getElementById('menu_link');
+const img = document.getElementById('menu_icon');
 
-
-/*
-logo.addEventListener('click', () => {
-  //Sjekker hvilken side man er på. Byttes til "folk.ntnu...osv"
-  if (site !== 'index.html') {
-    console.log('Du føres til siden index.html') //Endrer til at man føres til index.html
-  }
-})
-*/
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+function setIcon() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
+
+//Changes the menu-bar-image on hover
+menuButton.onmouseout = function() {
+  img.src = 'images/burger-menu-bars.png';
+};
+menuButton.onmouseover = function() {
+  img.src = 'images/burger-menu-bars-hover.png';
+};
+
 
 
 //----------------------------
