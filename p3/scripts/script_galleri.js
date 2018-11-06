0ac626bb3055ff66f4846ddc9dc2d50edd6ffd23
powni <magnusp99@hotmail.com>
2018-11-06 16:02:18 +0100

    Added navbar and footer to all

--- p3/scripts/script_galleri.js
+++ p3/scripts/script_galleri.js
@@ -50,3 +50,32 @@ function prevImage(){
   currentPic.setAttribute("value",i+"/"+image.length);
   slider_content.appendChild(currentPic);
 }
+
+//---------- NAVBAR ----------
+const logo = document.getElementById('logo');
+const site = window.location.href;
+const menuButton = document.getElementById('menu_link');
+const img = document.getElementById('menu_icon');
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function setIcon() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+//Changes the menu-bar-image on hover
+menuButton.onmouseout = function() {
+  img.src = 'images/burger-menu-bars.png';
+};
+menuButton.onmouseover = function() {
+  img.src = 'images/burger-menu-bars-hover.png';
+};
+
+
+
+//----------------------------
