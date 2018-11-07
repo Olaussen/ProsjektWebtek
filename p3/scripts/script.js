ffea9ee34ffe6f47e855b474a71619f8a8425bf0
powni <magnusp99@hotmail.com>
2018-11-07 19:31:10 +0100

    la til footer og navbar modulært med js

--- p3/scripts/script.js
+++ p3/scripts/script.js
@@ -5,7 +5,7 @@ let about = ['Om oss','About'];
 let menu = ['Meny','Menu'];
 let reservation = ['Reserver','Reservation'];
 let gallery = ['Galleri','Gallery'];
-i=0;
+i = 0; //ENDRER SPRÅK HER
 //Funksjon som skriver inn html-kode for navbaren
 function createNavbar() {
     document.open("text/html", "replace");
@@ -39,7 +39,7 @@ menuButton.onmouseover = function() {
 
 //---------- FOOTER ----------
 let footerText = ['<!-- Footer --><div class="footer"><a href="#wrapper" id="backToTop">Tilbake til toppen</a><div id="btmInfo"><span id="genInfo">The Fancy Pickle | Picklestreet 19 | 7042 Trondheim | </span><span id="tlfInfo">Tlf: 98765432 | </span><span id="infoMail"><a href="mailto:post@tfp.no" target="_top">POST@TFP.NO</a></span></div><p id="jobbInfo">Har du lyst på en jobb hos The Fancy Pickle? Vi er alltid interesserte i gode folk med som verdsetter service! Send oss en søknad med CV og et bilde av deg selv til jobb@tfp.no, eller så kan du sjekke om NAV har noen ledige stillinger <a href="https://tjenester.nav.no/stillinger/stillinger?q=the+fancy+pickle" target="_blank">HER</a></p></div></div>','<!-- Footer --><div class="footer"><a href="#wrapper" id="backToTop">Back to the top</a><div id="btmInfo"><span id="genInfo">The Fancy Pickle | Picklestreet 19 | 7042 Trondheim | </span><span id="tlfInfo">Phone:+47 98765432 | </span><span id="infoMail"><a href="mailto:post@tfp.no" target="_top">POST@TFP.NO</a></span></div><p id="jobbInfo">Do you want to work at The Fancy Pickle? We are always service-oriented persons! Send us an application with a CV and a picture of yourself to jobb@tfp.no, or you can check if NAV has some advertised jobs <a href="https://tjenester.nav.no/stillinger/stillinger?q=the+fancy+pickle" target="_blank">HERE</a></p></div>'];
-i = 0;
+
 
 function createFooter() {
     document.open("text/html", "replace");
