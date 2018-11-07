9fdd154dc3d534c654705b64de5fe90e1740e661
Lommesnusk <brage_schanche_pedersen@outlook.com>
2018-11-07 21:18:50 +0100

    lagt inn language script der det trengs, lagt over bordres til main.css og endret lang = en-us der det var no på engelske dokumenter

--- p3/scripts/script_galleri.js
+++ p3/scripts/script_galleri.js
@@ -10,11 +10,12 @@ let meny_link = ['meny.html','meny_eng.html'];
 let bordres_link = ['bordres.html','bordres_eng.html'];
 let galleri_link = ['galleri.html','galleri_eng.html'];
 i=0;
-
+if(window.location.pathname.slice(-8) == "eng.html"){i = 1;}
+else{i = 0;}
 //Funksjon som skriver inn html-kode for navbaren
 function createNavbar() {
     document.open("text/html", "replace");
-    document.write('<div class="navbar_logo"><img src="images/logo.png" alt="Picture of our logo" id="logo"></div><div class="topnav" id="myTopnav"><p id="placeholder"></p><a href='+about_link[i]+' id="om_oss">'+about[i]+'</a><a href='+meny_link[i]+' id="meny">'+menu[i]+'</a><a href='+bordres_link[i]+' id="reserver">'+reservation[i]+'</a><a href='+galleri_link[i]+' id="galleri">'+gallery[i]+'</a><a href="javascript:void(0);" id="menu_link" class="icon" onclick="setIcon()"><img id="menu_icon" src="images/burger-menu-bars.png" alt="Menu icon"></a></div>');
+    document.write('<div class="navbar_logo"><img src="images/logo.png" alt="Picture of our logo" id="logo"></div><div class="languageflags"><input type="image" id="noflag" src="images/noflag.png" name="noflag" alt="Norwegian flag" onclick="changelang_en_no()"><input type="image" id="usflag" src="images/usflag.png" name="usflag" alt="USA Flag" onclick="changelang_no_en()"></div><div class="topnav" id="myTopnav"><p id="placeholder"></p><a href='+about_link[i]+' id="om_oss">'+about[i]+'</a><a href='+meny_link[i]+' id="meny">'+menu[i]+'</a><a href='+bordres_link[i]+' id="reserver">'+reservation[i]+'</a><a href='+galleri_link[i]+' id="galleri">'+gallery[i]+'</a><a href="javascript:void(0);" id="menu_link" class="icon" onclick="setIcon()"><img id="menu_icon" src="images/burger-menu-bars.png" alt="Menu icon"></a></div>');
     document.close();
 }
 createNavbar();
@@ -108,4 +109,34 @@ function prevImage(){
   lol = setInterval(nextImage,3000);
   currentPic.setAttribute("value",i+"/"+image.length);
   slider_content.appendChild(currentPic);
+}
+
+/*--------Language script--------*/
+function changelang_no_en(){
+  if(window.location.pathname.slice(-12) == "bordres.html"){
+    window.location.assign(window.location.pathname.slice(0, -12) + "bordres_eng.html");
+  }
+  if(window.location.pathname.slice(-10) == "about.html"){
+    window.location.assign(window.location.pathname.slice(0, -10) + "about_eng.html");
+  }
+  if(window.location.pathname.slice(-9) == "meny.html"){
+    window.location.assign(window.location.pathname.slice(0, -9) + "meny_eng.html");
+  }
+  if(window.location.pathname.slice(-12) == "galleri.html"){
+    window.location.assign(window.location.pathname.slice(0, -12) + "galleri_eng.html");
+  }
+}
+function changelang_en_no(){
+  if(window.location.pathname.slice(-16) == "bordres_eng.html"){
+    window.location.assign(window.location.pathname.slice(0, -16) + "bordres.html");
+  }
+  if(window.location.pathname.slice(-14) == "about_eng.html"){
+    window.location.assign(window.location.pathname.slice(0, -14) + "about.html");
+  }
+  if(window.location.pathname.slice(-13) == "meny_eng.html"){
+    window.location.assign(window.location.pathname.slice(0, -13) + "meny.html");
+  }
+  if(window.location.pathname.slice(-16) == "galleri_eng.html"){
+    window.location.assign(window.location.pathname.slice(0, -16) + "galleri.html");
+  }
 }
